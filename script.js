37b70c0f2c1903a336945840a97c122659aae7d7
Klyaksonchik <162556249+Klyaksonchik@users.noreply.github.com>
2025-08-18 12:57:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -49,21 +49,8 @@ function updateImage() {
     tapEl.src = "fox5.png";
   }
 }
-// Запрещаем двойной тап увеличения
-tapEl.addEventListener('touchstart', function(event) {
-  if (event.touches.length > 20) {
-    event.preventDefault(); // запрещает pinch-zoom
-  }
-}, { passive: false });
 
-let lastTouch = 0;
-tapEl.addEventListener('touchend', function(event) {
-  const now = new Date().getTime();
-  if (now - lastTouch <= 300) {
-    event.preventDefault(); // запрещает double-tap zoom
-  }
-  lastTouch = now;
-}, { passive: false });
+
 // Функция генерации частиц (сердечки/звёздочки)
 function createParticles(event) {
   for (let i = 0; i < 5; i++) {
